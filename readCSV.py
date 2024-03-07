8b9b470887fdfa53550972243f9234ef6f2b6a3f
Xi LIU <xi.liu@u.nus.edu>
2024-03-07 15:42:03 +0800

    local

--- readCSV.py
+++ readCSV.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pathlib import Path
 import sys
 
+#file.py groupNum rowNum
 if len(sys.argv)!=3:
     print("wrong number of arguments")
 
@@ -9,6 +10,7 @@ group=int(sys.argv[1])
 rowNum=int(sys.argv[2])
 
 inParamFileName="./inParamsNew"+str(group)+".csv"
+# print("file name is "+inParamFileName)
 dfstr=pd.read_csv(inParamFileName)
 oneRow=dfstr.iloc[rowNum,:]
 
@@ -17,6 +19,9 @@ j2H=int(oneRow.loc["j2H"])
 g0=oneRow.loc["g0"]
 omegam=oneRow.loc["omegam"]
 omegap=oneRow.loc["omegap"]
+omegac=oneRow.loc["omegac"]
 er=oneRow.loc["er"]#magnification
-print("j1H"+str(j1H)+"j2H"+str(j2H)+"g0"+str(g0)+"omegam"+str(omegam)+"omegap"+str(omegap))
+thetaCoef=oneRow.loc["thetaCoef"]
+print("j1H"+str(j1H)+"j2H"+str(j2H)+"g0"+str(g0)+"omegam"+str(omegam)+"omegap"+str(omegap)+"omegac"+str(omegac)\
+      +"er"+str(er)+"thetaCoef"+str(thetaCoef))
 
